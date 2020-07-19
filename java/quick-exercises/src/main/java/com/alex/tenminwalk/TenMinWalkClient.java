7323e2d274485cfaec3f28472c47af9c1f4f1285
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-07-19 14:27:36 -0500

    add cool codewars examples

---
+++ java/quick-exercises/src/main/java/com/alex/tenminwalk/TenMinWalkClient.java
@@ -0,0 +1,9 @@
+package com.alex.tenminwalk;
+
+public class TenMinWalkClient {
+
+	public static void main(String[] args) {
+		System.out.println(TenMinWalk.isValid(new char[] { 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's' }));
+	}
+
+}
